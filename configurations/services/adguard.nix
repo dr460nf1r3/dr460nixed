d3807add1836e8fd7cefc9fc5f7d914ea77f178d
Nico Jensch <root@dr460nf1r3.org>
2023-04-28 16:38:28 +0000

    🔒 chore(adguard.nix): change option order to be alphabetic ✨ feat(adguard.nix): add support for custom /etc/hosts entries

--- configurations/services/adguard.nix
+++ configurations/services/adguard.nix
@@ -4,18 +4,18 @@
 }: {
   # Own systemd config using a custom user
   systemd.services.adguardhome = {
-    serviceConfig.User = "adguard";
-    serviceConfig.Group = "adguard";
     serviceConfig.DynamicUser = lib.mkForce false;
+    serviceConfig.Group = "adguard";
+    serviceConfig.User = "adguard";
   };
 
   # The custom adguard user & group
   users.users."adguard" = {
-    group = "adguard";
-    extraGroups = [ "nginx" ];
-    home = "/var/lib/AdGuardHome";
-    description = "Adguard DNS";
     createHome = false;
+    description = "Adguard DNS";
+    extraGroups = [ "nginx" ];
+    group = "adguard";
+    home = "/var/lib/AdGuardHome";
     isSystemUser = true;
   };
   users.groups.adguard = { };
@@ -25,77 +25,26 @@
     enable = true;
     mutableSettings = false;
     settings = {
+      auth_attempts = 5;
       bind_host = "127.0.0.1";
       bind_port = 3000;
-      users = [
-        {
-          name = "nico";
-          password = "$2b$05$zjtZ461DmcgZTgv7AppSOu66ZieCWEAR63xCeGQt.CyKJYAzhkK82";
-        }
-      ];
-      auth_attempts = 5;
       block_auth_min = 15;
       dns = {
+        aaaa_disabled = false;
+        all_servers = false;
+        allowed_clients = [ ];
+        anonymize_client_ip = false;
         bind_hosts = [
           "100.86.102.115"
           "fd7a:115c:a1e0:ab12:4843:cd96:6256:6673"
         ];
-        port = 53;
-        statistics_interval = 90;
-        querylog_enabled = true;
-        querylog_file_enabled = true;
-        querylog_interval = "2160h";
-        querylog_size_memory = 1000;
-        anonymize_client_ip = false;
-        protection_enabled = true;
-        blocking_mode = "default";
-        blocking_ipv4 = "";
-        blocking_ipv6 = "";
-        blocked_response_ttl = 10;
-        parental_block_host = "family-block.dns.adguard.com";
-        safebrowsing_block_host = "standard-block.dns.adguard.com";
-        ratelimit = 20;
-        ratelimit_whitelist = [ ];
-        refuse_any = true;
-        upstream_dns = [ "https://cloudflare-dns.com/dns-query" ];
-        upstream_dns_file = "";
-        bootstrap_dns = [
-          "1.1.1.1"
-          "1.0.0.1"
-          "2606:4700:4700::1111"
-          "2606:4700:4700::1001"
-        ];
-        all_servers = false;
-        fastest_addr = false;
-        fastest_timeout = "1s";
-        allowed_clients = [ ];
-        disallowed_clients = [ ];
         blocked_hosts = [
           "hostname.bind"
         ];
-        trusted_proxies = [
-          "127.0.0.1"
-        ];
-        cache_size = 4194304;
-        cache_ttl_min = 0;
-        cache_ttl_max = 0;
-        cache_optimistic = true;
-        bogus_nxdomain = [ ];
-        aaaa_disabled = false;
-        enable_dnssec = true;
-        edns_client_subnet = false;
-        max_goroutines = 300;
-        ipset = [ ];
-        filtering_enabled = true;
-        filters_update_interval = 1;
-        parental_enabled = false;
-        safesearch_enabled = false;
-        safebrowsing_enabled = false;
-        safebrowsing_cache_size = 1048576;
-        safesearch_cache_size = 1048576;
-        parental_cache_size = 1048576;
-        cache_time = 30;
-        rewrites = [ ];
+        blocked_response_ttl = 10;
+        blocking_ipv4 = "";
+        blocking_ipv6 = "";
+        blocking_mode = "default";
         blocked_services = [
           "facebook"
           "instagram"
@@ -106,87 +55,150 @@
           "twitter"
           "whatsapp"
         ];
-        upstream_timeout = "10s";
-        private_networks = [ ];
-        use_private_ptr_resolvers = true;
-        local_ptr_upstreams = [ ];
+        bogus_nxdomain = [ ];
+        bootstrap_dns = [
+          "1.1.1.1"
+          "1.0.0.1"
+          "2606:4700:4700::1111"
+          "2606:4700:4700::1001"
+        ];
+        cache_optimistic = true;
+        cache_size = 4194304;
+        cache_time = 30;
+        cache_ttl_max = 0;
+        cache_ttl_min = 0;
+        disallowed_clients = [ ];
         dns64_prefixes = [ ];
+        edns_client_subnet = false;
+        enable_dnssec = true;
+        fastest_addr = false;
+        fastest_timeout = "1s";
+        filtering_enabled = true;
+        filters_update_interval = 1;
+        ipset = [ ];
+        local_ptr_upstreams = [ ];
+        max_goroutines = 300;
+        parental_block_host = "family-block.dns.adguard.com";
+        parental_cache_size = 1048576;
+        parental_enabled = false;
+        port = 53;
+        private_networks = [ ];
+        protection_enabled = true;
+        querylog_enabled = true;
+        querylog_file_enabled = true;
+        querylog_interval = "2160h";
+        querylog_size_memory = 1000;
+        ratelimit = 20;
+        ratelimit_whitelist = [ ];
+        refuse_any = true;
+        rewrites = [ ];
+        safebrowsing_block_host = "standard-block.dns.adguard.com";
+        safebrowsing_cache_size = 1048576;
+        safebrowsing_enabled = false;
+        safesearch_cache_size = 1048576;
+        safesearch_enabled = false;
         serve_http3 = true;
+        statistics_interval = 90;
+        trusted_proxies = [
+          "127.0.0.1"
+        ];
+        upstream_dns = [ "https://cloudflare-dns.com/dns-query" ];
+        upstream_dns_file = "";
+        upstream_timeout = "10s";
         use_http3_upstreams = true;
+        use_private_ptr_resolvers = true;
       };
       querylog = {
         enabled = true;
         file_enabled = true;
+        ignored = [ ];
         interval = "2160h";
         size_memory = 1000;
-        ignored = [ ];
       };
       statistics = {
         enabled = true;
-        interval = 90;
         ignored = [ ];
+        interval = 90;
       };
       filters = [
         {
           # AdGuard Base filter, Social media filter, Spyware filter, Mobile ads filter, EasyList and EasyPrivacy
           enabled = true;
-          url = "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt";
-          name = "AdGuard DNS filter";
           id = 1;
+          name = "AdGuard DNS filter";
+          url = "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt";
         }
         {
           # AdAway default blocklist
           enabled = true;
-          url = "https://adaway.org/hosts.txt";
-          name = "AdAway Default Blocklist";
           id = 2;
+          name = "AdAway Default Blocklist";
+          url = "https://adaway.org/hosts.txt";
         }
         {
           # Dedicated no-tracking list
           enabled = true;
-          url = "https://github.com/notracking/hosts-blocklists/raw/master/adblock/adblock.txt";
-          name = "Anti-Tracking";
           id = 3;
+          name = "Anti-Tracking";
+          url = "https://github.com/notracking/hosts-blocklists/raw/master/adblock/adblock.txt";
         }
         {
           # Firehol Level 1 blocklist)
           enabled = true;
-          url = "https://iplists.firehol.org/files/firehol_level1.netset";
-          name = "Firehol Level 1";
           id = 4;
+          name = "Firehol Level 1";
+          url = "https://iplists.firehol.org/files/firehol_level1.netset";
         }
         {
           # Personal blocklist of Hazegi (mixing lots of popular blocklists)
           enabled = true;
-          url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/personal.txt";
-          name = "HaGeZi Personal Blocklist";
           id = 5;
+          name = "HaGeZi Personal Blocklist";
+          url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/personal.txt";
         }
         {
           enabled = true;
-          url = "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt";
-          name = "WindowsSpyBlocker";
           id = 6;
+          name = "WindowsSpyBlocker";
+          url = "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt";
         }
         {
           enabled = true;
-          url = "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/android-tracking.txt";
-          name = "Android Tracking";
           id = 7;
+          name = "Android Tracking";
+          url = "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/android-tracking.txt";
         }
       ];
       tls = {
-        enabled = true;
-        server_name = "100.86.102.115";
-        force_https = false;
-        port_https = 3001;
-        port_dns_over_tls = 853;
-        port_dns_over_quic = 853;
         allow_unencrypted_doh = true;
         certificate_path = config.sops.secrets."ssl/oracle-dragon-cert".path;
+        enabled = true;
+        force_https = false;
+        port_dns_over_quic = 853;
+        port_dns_over_tls = 853;
+        port_https = 3001;
         private_key_path = config.sops.secrets."ssl/oracle-dragon-key".path;
+        server_name = "100.86.102.115";
       };
+      users = [
+        {
+          name = "nico";
+          password = "$2b$05$zjtZ461DmcgZTgv7AppSOu66ZieCWEAR63xCeGQt.CyKJYAzhkK82";
+        }
+      ];
       schema_version = 14;
     };
+  };
+
+  # Supply non-RDNS found hostnames via /etc/hosts
+  networking.hosts = {
+    "100.116.167.11" = [ "dragon-pixel" ];
+    "100.85.210.126" = [ "rpi-dragon" ];
+    "100.86.102.115" = [ "oracle-dragon" ];
+    "100.96.236.126" = [ "slim-lair" ];
+    "fd7a:115c:a1e0:ab12:4843:cd96:6255:d27e" = [ "oracle-dragon" ];
+    "fd7a:115c:a1e0:ab12:4843:cd96:6256:6673" = [ "rpi-dragon" ];
+    "fd7a:115c:a1e0:ab12:4843:cd96:6260:ec7e" = [ "slim-lair" ];
+    "fd7a:115c:a1e0:ab12:4843:cd96:6274:a70b" = [ "dragon-pixel" ];
   };
 }
