4b8f23d8516afff8d68d98ce838ce320b0c7fd8f
Nico Jensch <root@dr460nf1r3.org>
2023-05-04 15:05:41 +0200

    🚀 feat(slim-lair.nix): change kernelPackages to linuxPackages_xanmod_latest 🚀 feat(slim-lair.nix): add radeontop and rocm-smi to systemPackages to monitor AMD GPU performance

--- hosts/slim-lair/slim-lair.nix
+++ hosts/slim-lair/slim-lair.nix
@@ -22,9 +22,9 @@
     # Needed to get the touchpad to work
     blacklistedKernelModules = [ "elan_i2c" ];
     # The new AMD Pstate driver & needed modules
-    extraModulePackages = with config.boot.kernelPackages; [ acpi_call zenpower ];
+    extraModulePackages = with config.boot.kernelPackages; [ amdgpu-pro acpi_call zenpower ];
     kernelModules = [ "acpi_call" "amdgpu" "amd-pstate=passive" ];
-    kernelPackages = pkgs.linuxPackages_cachyos;
+    kernelPackages = pkgs.linuxPackages_xanmod_latest;
     kernelParams = [ "initcall_blacklist=acpi_cpufreq_init" ];
     lanzaboote = {
       configurationLimit = 20;
@@ -101,7 +101,7 @@
   };
 
   # Enable the touchpad & secure boot, as well as add the ipman script
-  environment.systemPackages = with pkgs; [ libinput sbctl zenmonitor ];
+  environment.systemPackages = with pkgs; [ libinput radeontop rocm-smi sbctl zenmonitor ];
 
   # Neeeded for lzbt
   boot.bootspec.enable = true;
