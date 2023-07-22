f67deccf74ab8cfd2431159cc7f125b0b59ddb56
Nico Jensch <root@dr460nf1r3.org>
2023-07-23 00:26:03 +0200

    Back to cachyos; re-enable chaotic mesa-git

--- hosts/dragons-ryzen/dragons-ryzen.nix
+++ hosts/dragons-ryzen/dragons-ryzen.nix
@@ -18,9 +18,7 @@
     blacklistedKernelModules = [ "elan_i2c" ];
     # The new AMD Pstate driver & needed modules
     extraModulePackages = with config.boot.kernelPackages; [ acpi_call zenpower ];
-    kernelModules = [ "acpi_call" "amdgpu" "amd-pstate=passive" ];
-    kernelPackages = lib.mkForce pkgs.linuxPackages_xanmod;
-    kernelParams = [ "initcall_blacklist=acpi_cpufreq_init" ];
+    kernelModules = [ "acpi_call" "amdgpu" "amd_pstate=guided" "amd_iommu=off" ];
   };
 
   # Hostname
@@ -33,13 +31,8 @@
   services.hardware.bolt.enable = false;
   services.xserver.videoDrivers = [ "amdgpu" ];
 
-  # Fix the fucking Wifi - https://github.com/NixOS/nixpkgs/issues/18410
-  boot.extraModprobeConfig = ''
-    options iwlwifi power_save=0
-  '';
-
   # Bleeding edge Mesa - currently giving a slideshow
-  # chaotic.mesa-git.enable = true;
+  chaotic.mesa-git.enable = true;
 
   # Fix an issue
   programs.command-not-found.enable = false;
