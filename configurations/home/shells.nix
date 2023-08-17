3af84a142c65ca9f879a50cc4bb7333b1288955c
Nico Jensch <root@dr460nf1r3.org>
2023-08-17 20:27:16 +0200

    shells.nix: add new SSH connection with portforwarding (postgres)

--- configurations/home/shells.nix
+++ configurations/home/shells.nix
@@ -1,17 +1,21 @@
+let 
+  immortalis = "116.202.208.112";
+in
 {
   # SSH shortcuts
   programs = {
     bash.shellAliases = {
       "c" = "mosh nico@100.108.204.61";
       "g" = "mosh nico@100.75.73.33";
-      "g1" = "ssh -p 666 nico@116.202.208.112";
-      "g2" = "ssh nico@116.202.208.112";
-      "g3" = "ssh -p 223 nico@116.202.208.112";
-      "g4" = "ssh -p 224 nico@116.202.208.112";
-      "g5" = "ssh -p 225 nico@116.202.208.112";
-      "g6" = "ssh -p 226 nico@116.202.208.112";
-      "g7" = "ssh -p 224 nico@116.202.208.112";
-      "g8" = "ssh -p 222 nico@116.202.208.112";
+      "g1" = "ssh -p 666 nico@${immortalis}";
+      "g2" = "ssh nico@${immortalis}";
+      "g3" = "ssh -p 223 nico@${immortalis}";
+      "g4" = "ssh -p 224 nico@${immortalis}";
+      "g5" = "ssh -p 225 nico@${immortalis}";
+      "g6" = "ssh -p 226 nico@${immortalis}";
+      "g7" = "ssh -p 227 nico@${immortalis}";
+      "g8" = "ssh -p 222 nico@${immortalis}";
+      "g9" = "ssh -p 229 nico@${immortalis} -L 5432:127.0.0.1:5432";
       "m" = "mosh nico@100.109.201.47";
       "o" = "mosh nico@100.86.102.115";
       "w" = "mosh nico@100.64.127.121";
@@ -19,14 +23,15 @@
     fish.shellAbbrs = {
       "c" = "mosh nico@100.108.204.61";
       "g" = "mosh nico@100.75.73.33";
-      "g1" = "ssh -p 666 nico@116.202.208.112";
-      "g2" = "ssh nico@116.202.208.112";
-      "g3" = "ssh -p 223 nico@116.202.208.112";
-      "g4" = "ssh -p 224 nico@116.202.208.112";
-      "g5" = "ssh -p 225 nico@116.202.208.112";
-      "g6" = "ssh -p 226 nico@116.202.208.112";
-      "g7" = "ssh -p 224 nico@116.202.208.112";
-      "g8" = "ssh -p 222 nico@116.202.208.112";
+      "g1" = "ssh -p 666 nico@${immortalis}";
+      "g2" = "ssh nico@${immortalis}";
+      "g3" = "ssh -p 223 nico@${immortalis}";
+      "g4" = "ssh -p 224 nico@${immortalis}";
+      "g5" = "ssh -p 225 nico@${immortalis}";
+      "g6" = "ssh -p 226 nico@${immortalis}";
+      "g7" = "ssh -p 227 nico@${immortalis}";
+      "g8" = "ssh -p 222 nico@${immortalis}";
+      "g9" = "ssh -p 229 nico@${immortalis} -L 5432:127.0.0.1:5432";
       "m" = "mosh nico@100.109.201.47";
       "o" = "mosh nico@100.86.102.115";
       "w" = "mosh nico@100.64.127.121";
