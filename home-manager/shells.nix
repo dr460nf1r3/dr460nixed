507f2fd9d3e50f293fc5b76e34e1b5e2ec36545a
Nico Jensch <root@dr460nf1r3.org>
2023-09-09 09:46:17 +0200

    refactor(shells.nix): use more variables for commonly used strings and use Tailnet FQDN

--- home-manager/shells.nix
+++ home-manager/shells.nix
@@ -1,40 +1,43 @@
 let
-  immortalis = "116.202.208.112";
+  immortalis = "immortalis.kanyu-bushi.ts.net";
+  hetznerStoragebox = "your-storagebox.de";
+  forwardPostgres = "-L 5432:127.0.0.1:5432";
+  user = "nico";
 in
 {
   # SSH shortcuts
   programs = {
     bash.shellAliases = {
-      "c" = "mosh nico@100.108.204.61";
-      "g" = "mosh nico@100.75.73.33";
-      "g1" = "ssh -p 666 nico@${immortalis}";
-      "g2" = "ssh nico@${immortalis}";
-      "g3" = "ssh -p 223 nico@${immortalis}";
-      "g4" = "ssh -p 224 nico@${immortalis}";
-      "g5" = "ssh -p 225 nico@${immortalis}";
-      "g6" = "ssh -p 226 nico@${immortalis}";
-      "g7" = "ssh -p 227 nico@${immortalis}";
-      "g8" = "ssh -p 222 nico@${immortalis}";
-      "g9" = "ssh -p 229 nico@${immortalis} -L 5432:127.0.0.1:5432";
-      "m" = "mosh nico@100.109.201.47";
-      "o" = "mosh nico@100.86.102.115";
-      "w" = "mosh nico@100.64.127.121";
+      "b1" = "ssh -p23 u342919@u342919.${hetznerStoragebox}";
+      "b2" = "ssh -p23 u358867@u358867.${hetznerStoragebox}";
+      "g" = "mosh ${user}@google-dragon.emperor-mercat.ts.net";
+      "g1" = "ssh -p 666 ${user}@${immortalis}";
+      "g2" = "ssh ${user}@${immortalis}";
+      "g3" = "ssh -p 223 ${user}@${immortalis}";
+      "g4" = "ssh -p 224 ${user}@${immortalis}";
+      "g5" = "ssh -p 225 ${user}@${immortalis}";
+      "g6" = "ssh -p 226 ${user}@${immortalis}";
+      "g7" = "ssh -p 227 ${user}@${immortalis}";
+      "g8" = "ssh -p 222 ${user}@${immortalis}";
+      "g9" = "ssh -p 229 ${user}@${immortalis} ${forwardPostgres}";
+      "m" = "mosh ${user}@garuda-mail.kanyu-bushi.ts.net";
+      "o" = "mosh ${user}@oracle-dragon.emperor-mercat.ts.net";
     };
     fish.shellAbbrs = {
-      "c" = "mosh nico@100.108.204.61";
-      "g" = "mosh nico@100.75.73.33";
-      "g1" = "ssh -p 666 nico@${immortalis}";
-      "g2" = "ssh nico@${immortalis}";
-      "g3" = "ssh -p 223 nico@${immortalis}";
-      "g4" = "ssh -p 224 nico@${immortalis}";
-      "g5" = "ssh -p 225 nico@${immortalis}";
-      "g6" = "ssh -p 226 nico@${immortalis}";
-      "g7" = "ssh -p 227 nico@${immortalis}";
-      "g8" = "ssh -p 222 nico@${immortalis}";
-      "g9" = "ssh -p 229 nico@${immortalis} -L 5432:127.0.0.1:5432";
-      "m" = "mosh nico@100.109.201.47";
-      "o" = "mosh nico@100.86.102.115";
-      "w" = "mosh nico@100.64.127.121";
+      "b1" = "ssh -p23 u342919@u342919.${hetznerStoragebox}";
+      "b2" = "ssh -p23 u358867@u358867.${hetznerStoragebox}";
+      "g" = "mosh ${user}@google-dragon.emperor-mercat.ts.net";
+      "g1" = "ssh -p 666 ${user}@${immortalis}";
+      "g2" = "ssh ${user}@${immortalis}";
+      "g3" = "ssh -p 223 ${user}@${immortalis}";
+      "g4" = "ssh -p 224 ${user}@${immortalis}";
+      "g5" = "ssh -p 225 ${user}@${immortalis}";
+      "g6" = "ssh -p 226 ${user}@${immortalis}";
+      "g7" = "ssh -p 227 ${user}@${immortalis}";
+      "g8" = "ssh -p 222 ${user}@${immortalis}";
+      "g9" = "ssh -p 229 ${user}@${immortalis} ${forwardPostgres}";
+      "m" = "mosh ${user}@garuda-mail.kanyu-bushi.ts.net";
+      "o" = "mosh ${user}@oracle-dragon.emperor-mercat.ts.net";
     };
   };
 }
