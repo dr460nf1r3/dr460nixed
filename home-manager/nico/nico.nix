71970a82b8e96b46b7a10fc1e6d86f7caed6d6e9
Nico Jensch <root@dr460nf1r3.org>
2023-09-23 20:21:20 +0200

    fix(fish): fix fish configuration not getting generated, merge shell/nico.nix

--- home-manager/nico/nico.nix
+++ home-manager/nico/nico.nix
@@ -1,14 +1,52 @@
 {pkgs, ...}: let
   appdir = ".local/share/applications";
+  forwardPostgres = "-L 5432:127.0.0.1:5432";
+  hetznerStoragebox = "your-storagebox.de";
+  immortalis = "immortalis.kanyu-bushi.ts.net";
+  user = "nico";
 in {
   # Import individual configuration snippets
-  imports = [
-    ./email.nix
-    ./shells.nix
-  ];
+  imports = [./email.nix];
 
   # I'm working with git a lot
   programs = {
+    # Bash receives aliases
+    bash.shellAliases = {
+      "b1" = "ssh -p23 u342919@u342919.${hetznerStoragebox}";
+      "b2" = "ssh -p23 u358867@u358867.${hetznerStoragebox}";
+      "g" = "mosh ${user}@google-dragon.emperor-mercat.ts.net";
+      "g1" = "ssh -p 666 ${user}@${immortalis}";
+      "g2" = "ssh ${user}@${immortalis}";
+      "g3" = "ssh -p 223 ${user}@${immortalis}";
+      "g4" = "ssh -p 224 ${user}@${immortalis}";
+      "g5" = "ssh -p 225 ${user}@${immortalis}";
+      "g6" = "ssh -p 226 ${user}@${immortalis}";
+      "g7" = "ssh -p 227 ${user}@${immortalis}";
+      "g8" = "ssh -p 222 ${user}@${immortalis}";
+      "g9" = "ssh -p 229 ${user}@${immortalis} ${forwardPostgres}";
+      "m" = "mosh ${user}@garuda-mail.kanyu-bushi.ts.net";
+      "o" = "mosh ${user}@oracle-dragon.emperor-mercat.ts.net";
+    };
+    # Fish receives auto-expanding abbreviations (much cooler!)
+    fish = {
+      enable = true;
+      shellAbbrs = {
+        "b1" = "ssh -p23 u342919@u342919.${hetznerStoragebox}";
+        "b2" = "ssh -p23 u358867@u358867.${hetznerStoragebox}";
+        "g" = "mosh ${user}@google-dragon.emperor-mercat.ts.net";
+        "g1" = "ssh -p 666 ${user}@${immortalis}";
+        "g2" = "ssh ${user}@${immortalis}";
+        "g3" = "ssh -p 223 ${user}@${immortalis}";
+        "g4" = "ssh -p 224 ${user}@${immortalis}";
+        "g5" = "ssh -p 225 ${user}@${immortalis}";
+        "g6" = "ssh -p 226 ${user}@${immortalis}";
+        "g7" = "ssh -p 227 ${user}@${immortalis}";
+        "g8" = "ssh -p 222 ${user}@${immortalis}";
+        "g9" = "ssh -p 229 ${user}@${immortalis} ${forwardPostgres}";
+        "m" = "mosh ${user}@garuda-mail.kanyu-bushi.ts.net";
+        "o" = "mosh ${user}@oracle-dragon.emperor-mercat.ts.net";
+      };
+    };
     git = {
       signing = {
         key = "D245D484F3578CB17FD6DA6B67DB29BFF3C96757";
