01f6650022dffc6f840006a2f818ac3e35ecd3c7
Nico Jensch <root@dr460nf1r3.org>
2023-09-24 11:40:15 +0200

    refactor(flake.nix): sort inputs in another way, remove nix-snapshotter

--- flake.nix
+++ flake.nix
@@ -6,138 +6,178 @@
 
   inputs = {
     # Chaotic Nyx!
-    chaotic-nyx.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-    chaotic-nyx.inputs.home-manager.follows = "home-manager";
+    chaotic-nyx = {
+      url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+      inputs.home-manager.follows = "home-manager";
+    };
 
     # Devshell to set up a development environment
-    devshell.url = "github:numtide/devshell";
-    devshell.flake = false;
+    devshell = {
+      url = "github:numtide/devshell";
+      flake = false;
+    };
 
     # Disko for Nix-managed partition management
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Required by some other flakes
-    flake-compat.url = "github:edolstra/flake-compat";
-    flake-compat.flake = false;
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
 
     # Required by some other flakes
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
 
     # Required by pre-commit-hooks
     flake-utils.url = "github:numtide/flake-utils";
 
     # Garuda Linux flake - most of my system settings are here
-    # garuda-nix.url = "/home/nico/Documents/misc/garuda-nix-subsystem";
-    garuda-nix.url = "gitlab:garuda-linux/garuda-nix-subsystem/main";
-    garuda-nix.inputs.chaotic-nyx.follows = "chaotic-nyx";
-    garuda-nix.inputs.devshell.follows = "devshell";
-    garuda-nix.inputs.flake-parts.follows = "flake-parts";
-    garuda-nix.inputs.flake-utils.follows = "flake-utils";
-    garuda-nix.inputs.home-manager.follows = "home-manager";
-    garuda-nix.inputs.nix-index-database.follows = "nix-index-database";
-    garuda-nix.inputs.nixos-hardware.follows = "nixos-hardware";
-    garuda-nix.inputs.nixpkgs.follows = "nixpkgs";
-    garuda-nix.inputs.pre-commit-hooks.follows = "pre-commit-hooks";
-    garuda-nix.inputs.spicetify-nix.follows = "spicetify-nix";
+    garuda-nix = {
+      # url = "/home/nico/Documents/misc/garuda-nix-subsystem";
+      url = "gitlab:garuda-linux/garuda-nix-subsystem/main";
+      inputs.chaotic-nyx.follows = "chaotic-nyx";
+      inputs.devshell.follows = "devshell";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.home-manager.follows = "home-manager";
+      inputs.nix-index-database.follows = "nix-index-database";
+      inputs.nixos-hardware.follows = "nixos-hardware";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.pre-commit-hooks.follows = "pre-commit-hooks";
+      inputs.spicetify-nix.follows = "spicetify-nix";
+    };
 
     # Gitignore common input
-    gitignore.url = "github:hercules-ci/gitignore.nix";
-    gitignore.inputs.nixpkgs.follows = "nixpkgs";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Home-manager for managing my home directory
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Reset rootfs every reboot
     impermanence.url = "github:nix-community/impermanence";
 
     # My SSH keys
-    keys_nico.url = "https://github.com/dr460nf1r3.keys";
-    keys_nico.flake = false;
+    keys_nico = {
+      url = "https://github.com/dr460nf1r3.keys";
+      flake = false;
+    };
 
     # Lanzaboote for secure boot support
-    lanzaboote.url = "github:nix-community/lanzaboote/master";
-    lanzaboote.inputs.flake-compat.follows = "flake-compat";
-    lanzaboote.inputs.flake-parts.follows = "flake-parts";
-    lanzaboote.inputs.flake-utils.follows = "flake-utils";
-    lanzaboote.inputs.nixpkgs.follows = "nixpkgs";
-    lanzaboote.inputs.pre-commit-hooks-nix.follows = "pre-commit-hooks";
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/master";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.pre-commit-hooks-nix.follows = "pre-commit-hooks";
+    };
 
     # MicroVMs based on Nix
-    microvm.url = "github:astro/microvm.nix";
-    microvm.inputs.flake-utils.follows = "flake-utils";
-    microvm.inputs.nixpkgs.follows = "nixpkgs";
+    microvm = {
+      url = "github:astro/microvm.nix";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Nixd language server
-    nixd.url = "github:nix-community/nixd";
-    nixd.inputs.flake-parts.follows = "flake-parts";
-    nixd.inputs.nixpkgs.follows = "nixpkgs";
+    nixd = {
+      url = "github:nix-community/nixd";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Nix gaming-related packages and modules
-    nix-gaming.url = "github:fufexan/nix-gaming";
-    nix-gaming.inputs.flake-parts.follows = "flake-parts";
-    nix-gaming.inputs.nixpkgs.follows = "nixpkgs";
+    nix-gaming = {
+      url = "github:fufexan/nix-gaming";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Have a local index of nixpkgs for fast launching of apps
-    nix-index-database.url = "github:Mic92/nix-index-database";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Bring native understanding of Nix packages to containerd
-    nix-snapshotter.url = "github:pdtpartners/nix-snapshotter";
-    nix-snapshotter.inputs.flake-compat.follows = "flake-compat";
-    nix-snapshotter.inputs.flake-parts.follows = "flake-parts";
-    nix-snapshotter.inputs.nixpkgs.follows = "nixpkgs";
+    nix-index-database = {
+      url = "github:Mic92/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # feature-rich and convenient fork of the Nix package manager
-    nix-super.url = "github:privatevoid-net/nix-super";
-    nix-super.inputs.flake-compat.follows = "flake-compat";
-    nix-super.inputs.nixpkgs.follows = "nixpkgs";
+    nix-super = {
+      url = "github:privatevoid-net/nix-super";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    nixos-anywhere.url = "https://raw.githubusercontent.com/numtide/nixos-anywhere/main/src/nixos-anywhere.sh";
-    nixos-anywhere.flake = false;
+    nixos-anywhere = {
+      url = "https://raw.githubusercontent.com/numtide/nixos-anywhere/main/src/nixos-anywhere.sh";
+      flake = false;
+    };
 
     # NixOS generators to build system images
-    nixos-generators.url = "github:nix-community/nixos-generators";
-    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # NixOS hardware database
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
     # NixOS WSL
-    nixos-wsl.url = "github:nix-community/nixos-wsl";
-    nixos-wsl.inputs.flake-compat.follows = "flake-compat";
-    nixos-wsl.inputs.flake-utils.follows = "flake-utils";
-    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-wsl = {
+      url = "github:nix-community/nixos-wsl";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # The source of all truth!
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     # Easy linting of the flake and all kind of other stuff
-    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-    pre-commit-hooks.inputs.flake-compat.follows = "flake-compat";
-    pre-commit-hooks.inputs.flake-utils.follows = "flake-utils";
-    pre-commit-hooks.inputs.gitignore.follows = "gitignore";
-    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
-    pre-commit-hooks.inputs.nixpkgs-stable.follows = "nixpkgs-stable";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.gitignore.follows = "gitignore";
+      inputs.nixpkgs-stable.follows = "nixpkgs-stable";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Secrets management
-    sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
-    sops-nix.inputs.nixpkgs-stable.follows = "nixpkgs-stable";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs-stable";
+    };
 
     # Spicetify
-    spicetify-nix.url = "github:the-argus/spicetify-nix";
-    spicetify-nix.inputs.flake-utils.follows = "flake-utils";
-    spicetify-nix.inputs.nixpkgs.follows = "nixpkgs";
+    spicetify-nix = {
+      url = "github:the-argus/spicetify-nix";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # The Chaotic toolbox
-    src-chaotic-toolbox.url = "github:chaotic-aur/toolbox";
-    src-chaotic-toolbox.flake = false;
-    src-repoctl.url = "github:cassava/repoctl";
-    src-repoctl.flake = false;
+    src-chaotic-toolbox = {
+      url = "github:chaotic-aur/toolbox";
+      flake = false;
+    };
+    src-repoctl = {
+      url = "github:cassava/repoctl";
+      flake = false;
+    };
   };
 
   outputs = {
