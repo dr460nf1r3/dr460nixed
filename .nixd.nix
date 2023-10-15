d793ee52cf8b8a0ef0b27b361999458fbb33a008
Nico Jensch <root@dr460nf1r3.org>
2023-10-15 16:53:35 +0200

    feat(nixd): add nixd option evaluation and config file

---
+++ .nixd.nix
@@ -0,0 +1,10 @@
+{
+  formatting.command = "alejandra";
+  options = {
+    enable = true;
+    target = {
+      args = [];
+      installable = ".#nixosConfigurations.nixos.options";
+    };
+  };
+}
