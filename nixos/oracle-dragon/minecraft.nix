3bf56ee0c130a1402282af7ea21fb080b402ad14
Nico Jensch <root@dr460nf1r3.org>
2023-12-31 21:25:31 +0100

    feat(oracle-dragon): add Multiverse plugins to Minecraft

--- nixos/oracle-dragon/minecraft.nix
+++ nixos/oracle-dragon/minecraft.nix
@@ -17,7 +17,6 @@
         motd = "Welcome to Utopia <3";
         online-mode = true;
         resource-pack = "https://cdn.modrinth.com/data/r4GILswZ/versions/gWTrUifI/Faithful%2064x.zip";
-        resource-pack-prompt = "This installs the Faithful x64 texture pack.";
         resource-pack-sha1 = "88add430a01a1acb49a8e81bb96f7ba200bc80bf";
         server-port = 25565;
         snooper-enabled = false;
@@ -34,23 +33,23 @@
         };
       };
       symlinks = {
-        "plugins/ViaVersion.jar" = pkgs.fetchurl rec {
-          pname = "ViaVersion";
-          version = "4.9.2";
-          url = "https://github.com/ViaVersion/${pname}/releases/download/${version}/${pname}-${version}.jar";
-          hash = "sha256-jVBjs4hEV464xM0Edp1nGCefyIePG5/H7aavP/+9/o0=";
-        };
-        "plugins/ViaBackwards.jar" = pkgs.fetchurl rec {
-          pname = "ViaBackwards";
-          version = "4.9.1";
-          url = "https://github.com/ViaVersion/${pname}/releases/download/${version}/${pname}-${version}.jar";
-          hash = "sha256-j/mjgQwOn6C8VKtVcc/9gFSWzWLevaUwZDOZl7vXqGs=";
-        };
         "plugins/BlueMap.jar" = pkgs.fetchurl rec {
           pname = "BlueMap";
           version = "3.20";
           url = "https://hangarcdn.papermc.io/plugins/Blue/${pname}/versions/${version}/PAPER/${pname}-${version}-paper.jar";
           hash = "sha256-DXqxpYy6w/oYyBDmmky0ilbKFYzvkNmSv9B2tkCwFgU=";
+        };
+        "plugins/Chunky.jar" = pkgs.fetchurl rec {
+          pname = "Chunky";
+          version = "1.3.92";
+          url = "https://hangarcdn.papermc.io/plugins/pop4959/${pname}/versions/${version}/PAPER/${pname}-${version}.jar";
+          hash = "sha256-ABHfKJK0LQI2ZLt1D83897RAnE9xWu6+34IOlwTh17w=";
+        };
+        "plugins/EssentialsX.jar" = pkgs.fetchurl rec {
+          pname = "EssentialsX";
+          version = "2.20.1";
+          url = "https://github.com/EssentialsX/Essentials/releases/download/${version}/${pname}-${version}.jar";
+          hash = "sha256-gC6jC9pGDKRZfoGJJYFpM8EjsI2BJqgU+sKNA6Yb9UI=";
         };
         "plugins/Floodgate.jar" = let
           build = "87";
@@ -76,17 +75,35 @@
           url = "https://github.com/GeyserMC/Hurricane/releases/download/${version}/${pname}.jar";
           hash = "sha256-fDb+D94XLtSCcDM0DBQ6hDNhE+YVLti9Wo5sX1fkKk4=";
         };
-        "plugins/EssentialsX.jar" = pkgs.fetchurl rec {
-          pname = "EssentialsX";
-          version = "2.20.1";
-          url = "https://github.com/EssentialsX/Essentials/releases/download/${version}/${pname}-${version}.jar";
-          hash = "sha256-gC6jC9pGDKRZfoGJJYFpM8EjsI2BJqgU+sKNA6Yb9UI=";
+        "plugins/Multiverse-Core.jar" = pkgs.fetchurl rec {
+          pname = "Multiverse-Core";
+          version = "4.3.12";
+          url = "https://github.com/Multiverse/${pname}/releases/download/${version}/multiverse-core-${version}.jar";
+          hash = "sha256-mCN6rzXG7nv9lft/OZ73A7PnK/+Oq0iKkEqtnUUwzRA=";
         };
-        "plugins/Chunky.jar" = pkgs.fetchurl rec {
-          pname = "Chunky";
-          version = "1.3.92";
-          url = "https://hangarcdn.papermc.io/plugins/pop4959/${pname}/versions/${version}/PAPER/${pname}-${version}.jar";
-          hash = "sha256-ABHfKJK0LQI2ZLt1D83897RAnE9xWu6+34IOlwTh17w=";
+        "plugins/Multiverse-NetherPortals.jar" = pkgs.fetchurl rec {
+          pname = "Multiverse-NetherPortals";
+          version = "4.2.3";
+          url = "https://github.com/Multiverse/${pname}/releases/download/${version}/multiverse-netherportals-${version}.jar";
+          hash = "sha256-lyg5Vak62oL15wk4gDhII+IkZxl4uOZ2njwnuhWxusM=";
+        };
+        "plugins/Multiverse-Portals.jar" = pkgs.fetchurl rec {
+          pname = "Multiverse-Portals";
+          version = "4.2.3";
+          url = "https://github.com/Multiverse/${pname}/releases/download/${version}/multiverse-portals-${version}.jar";
+          hash = "sha256-6m5Qx3HZeCAh4LxJuVpO2hT7wv8gUfIx/Gruxkvl+aA=";
+        };
+        "plugins/ViaBackwards.jar" = pkgs.fetchurl rec {
+          pname = "ViaBackwards";
+          version = "4.9.1";
+          url = "https://github.com/ViaVersion/${pname}/releases/download/${version}/${pname}-${version}.jar";
+          hash = "sha256-j/mjgQwOn6C8VKtVcc/9gFSWzWLevaUwZDOZl7vXqGs=";
+        };
+        "plugins/ViaVersion.jar" = pkgs.fetchurl rec {
+          pname = "ViaVersion";
+          version = "4.9.2";
+          url = "https://github.com/ViaVersion/${pname}/releases/download/${version}/${pname}-${version}.jar";
+          hash = "sha256-jVBjs4hEV464xM0Edp1nGCefyIePG5/H7aavP/+9/o0=";
         };
       };
     };
