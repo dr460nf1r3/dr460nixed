efc545abed910604686b30a2d07fabe77dcc6420
Nico Jensch <root@dr460nf1r3.org>
2024-01-14 17:35:24 +0100

    feat(shells): add alias/abbr for qemu-kvm booting usb drive

--- nixos/modules/shells.nix
+++ nixos/modules/shells.nix
@@ -20,12 +20,33 @@ in {
     # Programs & global config
     programs = {
       bash.shellAliases = {
+        "bootsda" = ''
+          sudo qemu-kvm \
+          -m 4G \
+          -drive file=/dev/sda,format=raw,index=0,media=disk \
+          -net user,hostfwd=tcp:127.0.0.1:2222-:22 \
+          -net nic
+        '';
+        "bootsdb" = ''
+          sudo qemu-kvm
+          -m 4G \
+          -drive file=/dev/sdb,format=raw,index=0,media=disk \
+          -net user,hostfwd=tcp:127.0.0.1:2222-:22 \
+          -net nic
+        '';
         "gpl" = "${pkgs.curl}/bin/curl https://www.gnu.org/licenses/gpl-3.0.txt -o LICENSE";
         "grep" = "${pkgs.ugrep}/bin/ugrep";
         "nix" = "${pkgs.nix}/bin/nix --verbose --print-build-logs"; # https://github.com/NixOS/nix/pull/8323
       };
       fish = {
         shellAbbrs = {
+          "bootusb" = ''
+            sudo qemu-kvm \
+            -m 4G \
+            -drive file=/dev/sda,format=raw,index=0,media=disk \
+            -net user,hostfwd=tcp:127.0.0.1:2222-:22 \
+            -net nic
+          '';
           "gpl" = "${pkgs.curl}/bin/curl https://www.gnu.org/licenses/gpl-3.0.txt -o LICENSE";
         };
         shellAliases = {
