815e4b35db90e3022bae57c04ff052ea7f9003ef
Nico Jensch <root@dr460nf1r3.org>
2024-03-05 12:45:53 +0100

    chore(launchers): drop desktop items which got recently themed by Beautyline

--- home-manager/theme-launchers.nix
+++ home-manager/theme-launchers.nix
@@ -51,23 +51,6 @@ in {
       X-KDE-SubstituteUID=false
       X-KDE-Username=
     '';
-    "${appdir}/fish.desktop".text = ''
-      [Desktop Entry]
-      Categories=ConsoleOnly;System;
-      Comment=The user-friendly command line shell
-      Exec=${pkgs.fish}/bin/fish
-      Icon=fish
-      Name=Fish
-      NoDisplay=false
-      Path=
-      StartupNotify=true
-      Terminal=true
-      TerminalOptions=
-      Type=Application
-      Version=1.0
-      X-KDE-SubstituteUID=false
-      X-KDE-Username=
-    '';
     "${appdir}/rustdesk.desktop".source = "${pkgs.makeDesktopItem {
       name = "rustdesk";
       exec = "rustdesk";
@@ -77,63 +60,6 @@ in {
       genericName = "Remote Desktop";
       categories = ["Network"];
     }}/share/applications/rustdesk.desktop";
-    "${appdir}/keybase.desktop".text = ''
-      [Desktop Entry]
-      Categories=Network;
-      Comment=An Electron GUI for Keybase
-      Exec=${pkgs.keybase}/bin/keybase-gui %u
-      Icon=keyring-manager
-      MimeType=x-scheme-handler/web+stellar;x-scheme-handler/keybase;application/x-saltpack;
-      Name=Keybase
-      NoDisplay=false
-      Path=
-      StartupNotify=false
-      Terminal=false
-      TerminalOptions=
-      Type=Application
-      X-KDE-SubstituteUID=false
-      X-KDE-Username=
-    '';
-    "${appdir}/gimp.desktop".text = ''
-      [Desktop Entry]
-      Categories=Graphics;2DGraphics;RasterGraphics;GTK;
-      Comment=Create images and edit photographs
-      Exec=${pkgs.gimp}/bin/gimp-2.10 %U
-      GenericName=Image Editor
-      Icon=gimp
-      Keywords=GIMP;graphic;design;illustration;painting;
-      MimeType=image/bmp;image/g3fax;image/gif;image/x-fits;image/x-pcx;image/x-portable-anymap;image/x-portable-bitmap;image/x-portable-graymap;image/x-portable-pixmap;image/x-psd;image/x-sgi;image/x-tga;image/x-xbitmap;image/x-xwindowdump;image/x-xcf;image/x-compressed-xcf;image/x-gimp-gbr;image/x-gimp-pat;image/x-gimp-gih;image/x-sun-raster;image/tiff;image/jpeg;image/x-psp;application/postscript;image/png;image/x-icon;image/x-xpixmap;image/x-exr;image/webp;image/x-webp;image/heif;image/heic;image/avif;image/jxl;image/svg+xml;application/pdf;image/x-wmf;image/jp2;image/x-xcursor;
-      Name=GIMP
-      NoDisplay=false
-      Path=
-      StartupNotify=true
-      Terminal=false
-      TerminalOptions=
-      TryExec=gimp-2.10
-      Type=Application
-      Version=1.0
-      X-KDE-SubstituteUID=false
-      X-KDE-Username=
-    '';
-    "${appdir}/micro.desktop".text = ''
-      [Desktop Entry]
-      Categories=Utility;TextEditor;Development;
-      Comment=Edit text files in a terminal
-      Exec=${pkgs.micro}/bin/micro %F
-      GenericName=Text Editor
-      Icon=text-editor
-      Keywords=text;editor;syntax;terminal;
-      MimeType=text/plain;text/x-chdr;text/x-csrc;text/x-c++hdr;text/x-c++src;text/x-java;text/x-dsrc;text/x-pascal;text/x-perl;text/x-python;application/x-php;application/x-httpd-php3;application/x-httpd-php4;application/x-httpd-php5;application/xml;text/html;text/css;text/x-sql;text/x-diff;
-      Name=Micro
-      NoDisplay=false
-      Path=
-      StartupNotify=false
-      Terminal=true
-      TerminalOptions=
-      Type=Application
-      X-KDE-SubstituteUID=false
-      X-KDE-Username=
-    '';
     "${appdir}/org.pipewire.Helvum.desktop".text = ''
       [Desktop Entry]
       Categories=AudioVideo;Audio;Video;Midi;Settings;GNOME;GTK;
