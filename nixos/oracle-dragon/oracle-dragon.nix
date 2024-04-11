4a4aaab21a181bfd7cf9333e40ac97d54221453b
Nico Jensch <root@dr460nf1r3.org>
2024-04-11 10:27:39 +0200

    feat(oracle-dragon): enable IPv4/IPv6 packet forwarding for app connectors

--- nixos/oracle-dragon/oracle-dragon.nix
+++ nixos/oracle-dragon/oracle-dragon.nix
@@ -102,5 +102,11 @@
     siteUrl = "https://collab.dr460nf1r3.org";
   };
 
+  # Required for using app connectors in Tailscale
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
+  };
+
   system.stateVersion = "22.11";
 }
