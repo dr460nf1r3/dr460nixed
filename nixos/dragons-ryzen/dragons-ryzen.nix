331a59ae53685971145e8e17587011efe623c296
Nico Jensch <root@dr460nf1r3.org>
2024-04-30 09:56:32 +0200

    feat(dragons-ryzen): autologin, strip DM, mesa-git, BTRFS maintenance

--- nixos/dragons-ryzen/dragons-ryzen.nix
+++ nixos/dragons-ryzen/dragons-ryzen.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   config,
   inputs,
   pkgs,
@@ -15,7 +16,6 @@
   boot = {
     # Needed to get the touchpad working
     blacklistedKernelModules = ["elan_i2c"];
-    extraModulePackages = with config.boot.kernelPackages; [zenpower];
     supportedFilesystems = ["btrfs"];
   };
 
@@ -24,22 +24,28 @@
 
   # The services to use on this machine
   services = {
+    # This is going to be a device with autologin
+    # straight to desktop. No display manager needed.
+    displayManager = {
+      enable = lib.mkForce false;
+      sddm.enable = false;
+    };
+    getty.autologinUser = "nico";
     hardware.bolt.enable = false;
     pipewire.lowLatency = {
       enable = true;
       quantum = 64;
       rate = 48000;
     };
-    displayManager.sddm.settings = {
-      Autologin = {
-        Session = "plasma";
-        User = "nico";
-      };
-    };
-    xserver = {
-      videoDrivers = ["amdgpu"];
-    };
+    xserver.enable = false;
   };
+
+  # Needed for autologin in TTY to start the desktop session
+  programs.bash.loginShellInit = ''
+    if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
+        exec startplasma-wayland
+    fi
+  '';
 
   # Enable a few selected custom settings
   dr460nixed = {
@@ -69,6 +75,22 @@
     yubikey = true;
   };
 
+  # Garuda Nix subsystem modules
+  garuda.btrfs-maintenance = {
+    deduplication = true;
+    enable = true;
+    uuid = "7f894697-a4e9-43a7-bdd8-00c0376ce1f9";
+  };
+
+  # Chaotic Nyx stuff
+  chaotic = {
+    mesa-git.enable = true;
+    scx = {
+      enable = true;
+      scheduler = "scx_rustland";
+    };
+  };
+
   # RADV video decode & general usage
   environment.variables = {
     AMD_VULKAN_ICD = "RADV";
@@ -76,18 +98,23 @@
   };
 
   # Enable the touchpad & secure boot, as well as add the ipman script
-  environment.systemPackages = with pkgs; [libinput radeontop zenmonitor];
+  environment.systemPackages = with pkgs; [libinput radeontop scx];
 
   # Home-manager individual settings
   home-manager.users."nico" = import ../../home-manager/nico/nico.nix;
 
   # A few secrets
   sops.secrets = {
-    # "ssh_keys/id_rsa" = {
-    #   mode = "0600";
-    #   owner = config.users.users.nico.name;
-    #   path = "/home/nico/.ssh/id_rsa";
-    # };
+    "ssh_keys/id_rsa" = {
+      mode = "0600";
+      owner = config.users.users.nico.name;
+      path = "/home/nico/.ssh/id_rsa";
+    };
+    "ssh_keys/id_ed25519" = {
+      mode = "0600";
+      owner = config.users.users.nico.name;
+      path = "/home/nico/.ssh/id_ed25519";
+    };
   };
 
   # For some reason Bluetooth only works after un-/reloading
