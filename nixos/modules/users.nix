ff45069a795a9d2f862381fe669f676bdcb65840
Nico Jensch <root@dr460nf1r3.org>
2024-05-01 18:26:13 +0200

    feat(users): allow certain actions without sudo

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -92,6 +92,40 @@ in {
     };
   };
 
+  security.sudo.extraRules = [
+    {
+      # allow wheel group to run nixos-rebuild without password
+      groups = ["wheel"];
+      commands = let
+        currentSystem = "/run/current-system/";
+        storePath = "/nix/store/";
+      in [
+        {
+          command = "${storePath}/*/bin/switch-to-configuration";
+          options = ["SETENV" "NOPASSWD"];
+        }
+        {
+          command = "${currentSystem}/sw/bin/nix-store";
+          options = ["SETENV" "NOPASSWD"];
+        }
+        {
+          command = "${currentSystem}/sw/bin/nixos-rebuild";
+          options = ["NOPASSWD"];
+        }
+        {
+          # let wheel group collect garbage without password
+          command = "${currentSystem}/sw/bin/nix-collect-garbage";
+          options = ["SETENV" "NOPASSWD"];
+        }
+        {
+          # let wheel group interact with systemd without password
+          command = "${currentSystem}/sw/bin/systemctl";
+          options = ["NOPASSWD"];
+        }
+      ];
+    }
+  ];
+
   # Allow pushing to Cachix
   # sops.secrets."api_keys/cachix" = {
   #   mode = "0600";
