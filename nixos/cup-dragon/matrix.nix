d173e809c6cb38447a24475e4f81d8a721e72a88
Nico Jensch <root@dr460nf1r3.org>
2024-11-16 08:04:46 +0100

    fix(matrix): serve delegation via CF

--- nixos/cup-dragon/matrix.nix
+++ nixos/cup-dragon/matrix.nix
@@ -4,27 +4,10 @@
   ...
 }:
 # Adapted from:
-# https://gitlab.com/famedly/conduit/-/blob/3bfdae795d4d9ec9aeaac7465e7535ac88e47756/nix/README.md
+# https://gitlab.com/famedly/conduit/-/blo  b/3bfdae795d4d9ec9aeaac7465e7535ac88e47756/nix/README.md
 let
   server_name = "dr460nf1r3.org";
   matrix_hostname = "matrix.${server_name}";
-
-  well_known_server = pkgs.writeText "well-known-matrix-server" ''
-    {
-      "m.server": "${matrix_hostname}"
-    }
-  '';
-
-  well_known_client = pkgs.writeText "well-known-matrix-client" ''
-    {
-      "m.homeserver": {
-        "base_url": "https://${matrix_hostname}"
-      },
-      "org.matrix.msc3575.proxy": {
-        "url": "https://matrix.dr460nf1r3.org"
-      }
-    }
-  '';
 in {
   services.matrix-conduit = {
     enable = true;
@@ -37,59 +20,48 @@ in {
     };
   };
   services.nginx = {
-    virtualHosts."${matrix_hostname}" = {
-      extraConfig = ''
-        merge_slashes off;
-      '';
-      forceSSL = true;
-      http3 = true;
-      http3_hq = true;
-      kTLS = true;
-      listen = [
-        {
-          addr = "0.0.0.0";
-          port = 443;
-          ssl = true;
-        }
-        {
-          addr = "[::]";
-          port = 443;
-          ssl = true;
-        }
-        {
-          addr = "0.0.0.0";
-          port = 8448;
-          ssl = true;
-        }
-        {
-          addr = "[::]";
-          port = 8448;
-          ssl = true;
-        }
-      ];
-      locations."/_matrix/" = {
-        proxyPass = "http://backend_conduit$request_uri";
-        proxyWebsockets = true;
+    virtualHosts = {
+      "${matrix_hostname}" = {
         extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_buffering off;
+          merge_slashes off;
         '';
+        forceSSL = true;
+        http3 = true;
+        http3_hq = true;
+        kTLS = true;
+        listen = [
+          {
+            addr = "0.0.0.0";
+            port = 443;
+            ssl = true;
+          }
+          {
+            addr = "[::]";
+            port = 443;
+            ssl = true;
+          }
+          {
+            addr = "0.0.0.0";
+            port = 8448;
+            ssl = true;
+          }
+          {
+            addr = "[::]";
+            port = 8448;
+            ssl = true;
+          }
+        ];
+        locations."/_matrix/" = {
+          proxyPass = "http://backend_conduit$request_uri";
+          proxyWebsockets = true;
+          extraConfig = ''
+            proxy_set_header Host $host;
+            proxy_buffering off;
+          '';
+        };
+        quic = true;
+        useACMEHost = "dr460nf1r3.org";
       };
-      locations."=/.well-known/matrix/server" = {
-        alias = "${well_known_server}";
-        extraConfig = ''
-          default_type application/json;
-        '';
-      };
-      locations."=/.well-known/matrix/client" = {
-        alias = "${well_known_client}";
-        extraConfig = ''
-          default_type application/json;
-          add_header Access-Control-Allow-Origin "*";
-        '';
-      };
-      quic = true;
-      useACMEHost = "dr460nf1r3.org";
     };
 
     upstreams = {
