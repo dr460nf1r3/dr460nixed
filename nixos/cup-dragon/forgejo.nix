f1a75ecb25bfb1d74eb7b213508e6e31bf4f1cda
Nico Jensch <root@dr460nf1r3.org>
2025-07-27 15:30:33 +0200

    feat(cup-dragon): fuck AI crawlers, enable anubis

--- nixos/cup-dragon/forgejo.nix
+++ nixos/cup-dragon/forgejo.nix
@@ -5,12 +5,6 @@
   ...
 }: let
   cfg = config.services.forgejo;
-  srv = cfg.settings.server;
-  theme = pkgs.fetchzip {
-    url = "https://github.com/catppuccin/gitea/releases/download/v1.0.1/catppuccin-gitea.tar.gz";
-    sha256 = "sha256-et5luA3SI7iOcEIQ3CVIu0+eiLs8C/8mOitYlWQa/uI=";
-    stripRoot = false;
-  };
   woodpecker-server = "ci.dr460nf1r3.org";
 in {
   services.nginx = {
@@ -23,7 +17,12 @@ in {
         http3 = true;
         http3_hq = true;
         kTLS = true;
-        locations."/".proxyPass = "http://localhost:${toString srv.HTTP_PORT}";
+        locations."/" = {
+          recommendedProxySettings = true;
+          proxyPass =
+            "http://unix:"
+            + config.services.anubis.instances.forgejo.settings.BIND;
+        };
         quic = true;
         useACMEHost = "dr460nf1r3.org";
       };
@@ -36,32 +35,60 @@ in {
     lfs.enable = true;
     package = pkgs.forgejo;
     settings = {
-      actions.ENABLED = true;
+      actions = {
+        ENABLED = false;
+        DEFAULT_ACTIONS_URL = "https://code.forgejo.org";
+      };
+      api.ENABLE_SWAGGER = false;
+      attachment.ALLOWED_TYPES = "*/*";
       DEFAULT = {
         APP_NAME = "Dragon's Git forge";
         APP_SLOGAN = "The place where dragons forge their code";
       };
+      federation.ENABLED = true;
       mailer = {
         ENABLED = true;
         FROM = "noreply@dr460nf1r3.org";
         PASSWD = config.sops.secrets."mail/forgejo".path;
+        PROTOCOL = "smtps";
         SMTP_ADDR = "mail.garudalinux.net";
+        SMTP_PORT = 465;
         USER = "noreply@dr460nf1r3.org";
       };
       other = {
-        SHOW_FOOTER_VERSION = true;
+        SHOW_FOOTER_VERSION = false;
+        SHOW_FOOTER_TEMPLATE_LOAD_TIME = false;
+        ENABLE_FEED = false;
       };
       server = {
-        DOMAIN = "git.dr460nf1r3.org";
+        PROTOCOL = "http+unix";
+        ROOT_URL = "https://${config.services.forgejo.settings.server.DOMAIN}";
         HTTP_PORT = 3050;
-        ROOT_URL = "https://${srv.DOMAIN}/";
+        DOMAIN = "git.dr460nf1r3.org";
+
+        BUILTIN_SSH_SERVER_USER = "git";
+        DISABLE_ROUTER_LOG = true;
+
+        START_SSH_SERVER = true;
+        SSH_CREATE_AUTHORIZED_KEYS_FILE = true;
+        SSH_PORT = 2222;
+        SSH_LISTEN_PORT = 2222;
+        OFFLINE_MODE = false;
       };
       service.DISABLE_REGISTRATION = true;
       session.COOKIE_SECURE = true;
-      ui = {
-        DEFAULT_THEME = "catppuccin-maroon-auto";
-        THEMES = "catppuccin-rosewater-auto,catppuccin-flamingo-auto,catppuccin-pink-auto,catppuccin-mauve-auto,catppuccin-red-auto,catppuccin-maroon-auto,catppuccin-peach-auto,catppuccin-yellow-auto,catppuccin-green-auto,catppuccin-teal-auto,catppuccin-sky-auto,catppuccin-sapphire-auto,catppuccin-blue-auto,catppuccin-lavender-auto";
+
+      "ui.meta" = {
+        AUTHOR = "dr460nf1r3";
+        DESCRIPTION = "Dragon's Git forge, a self-hosted Forgejo instance";
+        KEYWORDS = "git,self-hosted,gitea,forgejo,dr460nf1r3,garudalinux,garuda-linux,open-source,nix,nixos,chaotic-aur,chaotic";
       };
+    };
+  };
+
+  services.anubis = {
+    instances.forgejo.settings = {
+      TARGET = "unix://${config.services.forgejo.settings.server.HTTP_ADDR}";
     };
   };
 
@@ -84,31 +111,7 @@ in {
   in ''
     ${adminCmd} create --admin --email "root@localhost" --username ${user} --password "$(tr -d '\n' < ${pwd.path})" || true
     # ${adminCmd} change-password --username ${user} --password "$(tr -d '\n' < ${pwd.path})" || true
-
-    rm -rf ${config.services.forgejo.stateDir}/custom/public/assets
-    mkdir -p ${config.services.forgejo.stateDir}/custom/public/assets
-    ln -sf ${theme} ${config.services.forgejo.stateDir}/custom/public/assets/css
   '';
-
-  # Runner for Forgejo Actions
-  services.gitea-actions-runner = {
-    package = pkgs.forgejo-actions-runner;
-    instances.default = {
-      enable = true;
-      name = "cup-dragon-1";
-      url = "https://git.dr460nf1r3.org";
-      tokenFile = config.sops.secrets."api_keys/forgejo_runner".path;
-      labels = [
-        "ubuntu-latest:docker://node:16-bullseye"
-        "ubuntu-22.04:docker://node:16-bullseye"
-        "native:host"
-      ];
-    };
-  };
-  sops.secrets."api_keys/forgejo_runner" = {
-    mode = "0400";
-    path = "/var/lib/gitea-runner/.token";
-  };
 
   # Woodpecker CI/CD, just because why not?
   services.woodpecker-server = {
@@ -128,15 +131,13 @@ in {
     path = "/var/lib/woodpecker/.env";
   };
 
-  services.woodpecker-agents.agents."docker" = {
-    enable = true;
-    extraGroups = ["podman"];
-    environment = {
-      DOCKER_HOST = "unix:///run/podman/podman.sock";
-      WOODPECKER_BACKEND = "docker";
-      WOODPECKER_MAX_WORKFLOWS = "4";
-      WOODPECKER_SERVER = "localhost:9000";
+  users = {
+    groups.git = {};
+    users.git = {
+      isSystemUser = true;
+      createHome = false;
+      group = "git";
     };
-    environmentFile = [config.sops.secrets."env/woodpecker".path];
+    users.nginx.extraGroups = [config.users.groups.anubis.name];
   };
 }
