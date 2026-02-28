b364f1cd59f68a601644729a72c523b3dbbda1d9
dr460nf1r3 <dr460nf1r3@users.noreply.github.com>
2026-02-28 16:41:42 +0000

    deploy: 5201d196974d8c8a2cc25325e2507fcd141f6a64

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-af691922.js',
+                path_to_root + 'searchindex-040deef1.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
